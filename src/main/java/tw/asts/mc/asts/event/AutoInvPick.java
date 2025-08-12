7111e4761167b4fc49a90c0959bcddea5fd68482
AS_Allen_Shih <65409535+ASAllenShih@users.noreply.github.com>
2025-08-12 23:04:10 +0800

    自動重新種植機制

--- src/main/java/tw/asts/mc/asts/event/AutoInvPick.java
+++ src/main/java/tw/asts/mc/asts/event/AutoInvPick.java
@@ -1,6 +1,8 @@
 package tw.asts.mc.asts.event;
 
 import org.bukkit.Material;
+import org.bukkit.Tag;
+import org.bukkit.block.data.Ageable;
 import org.bukkit.entity.Item;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockDropItemEvent;
@@ -16,26 +18,42 @@ final public class AutoInvPick {
     private final List<Material> dropBlock;
     public AutoInvPick(UserConfig userConfig) {
         this.userConfig = userConfig;
-        List<String> colors = List.of("white", "light_gray", "gray", "black", "brown", "red", "orange", "yellow", "lime", "green", "cyan", "light_blue", "blue", "purple", "magenta", "pink");
-        List<String> colorsBlock = List.of("stained_glass", "stained_glass_pane");
-        List<Material> normalBlock = List.of(Material.BOOKSHELF, Material.TINTED_GLASS, Material.SEA_LANTERN, Material.ENDER_CHEST, Material.BUDDING_AMETHYST, Material.PACKED_ICE, Material.BLUE_ICE, Material.ICE, Material.GLASS, Material.GLASS_PANE);
+        final List<String> colors = List.of("white", "light_gray", "gray", "black", "brown", "red", "orange", "yellow", "lime", "green", "cyan", "light_blue", "blue", "purple", "magenta", "pink");
+        final List<String> colorsBlock = List.of("stained_glass", "stained_glass_pane");
+        final List<Material> normalBlock = List.of(Material.BOOKSHELF, Material.TINTED_GLASS, Material.SEA_LANTERN, Material.ENDER_CHEST, Material.BUDDING_AMETHYST, Material.PACKED_ICE, Material.BLUE_ICE, Material.ICE, Material.GLASS, Material.GLASS_PANE);
         dropBlock = Stream.concat(colorsBlock.stream().map(block -> colors.stream().map(color -> Material.getMaterial((color + "_" + block).toUpperCase())).toList()).flatMap(List::stream), normalBlock.stream()).filter(Objects::nonNull).toList();
     }
     public void onBlockBreak(@NotNull BlockBreakEvent event) {
         if (userConfig.config.isSet("auto_inv_pick." + event.getPlayer().getName()) && !userConfig.config.getBoolean("auto_inv_pick." + event.getPlayer().getName())) {
             return;
         }
-        int experience = event.getExpToDrop();
+        final int experience = event.getExpToDrop();
         event.setExpToDrop(0);
         if (dropBlock.contains(event.getBlock().getType())) {
-            ItemStack blockItem = new ItemStack(event.getBlock().getType(), 1);
-            HashMap<Integer, ItemStack> remaining = event.getPlayer().getInventory().addItem(blockItem);
+            final ItemStack blockItem = new ItemStack(event.getBlock().getType(), 1);
+            final HashMap<Integer, ItemStack> remaining = event.getPlayer().getInventory().addItem(blockItem);
             remaining.values().forEach(item -> event.getBlock().getWorld().dropItemNaturally(event.getBlock().getLocation(), item));
             if (experience > 0) {
                 event.getPlayer().giveExp(experience, true);
             }
             event.setDropItems(false);
             return;
+        } else if (Tag.CROPS.isTagged(event.getBlock().getType())) {
+            final Ageable ageable = (Ageable) event.getBlock().getBlockData();
+            if (ageable.getAge() == ageable.getMaximumAge()) {
+                event.setCancelled(true);
+                event.getBlock().getDrops(event.getPlayer().getActiveItem(), event.getPlayer()).forEach(itemStack -> {
+                    final HashMap<Integer, ItemStack> remaining = event.getPlayer().getInventory().addItem(itemStack);
+                    remaining.values().forEach(item -> event.getBlock().getWorld().dropItemNaturally(event.getBlock().getLocation(), item));
+                });
+                final Material ageableMaterial = ageable.getMaterial();
+                final Material placementMaterial = ageable.getPlacementMaterial();
+                event.getBlock().setType(Material.AIR, true);
+                if (event.getPlayer().getInventory().contains(placementMaterial)) {
+                    event.getPlayer().getInventory().removeItem(new ItemStack(placementMaterial, 1));
+                    event.getBlock().setType(ageableMaterial, true);
+                }
+            }
         }
         if (experience > 0) {
             event.getPlayer().giveExp(experience, true);
