a1d917e51e1b954e4404c77fad714d9d3ac6cc6b
cutebear0123 <cutebear@0w0b.dev>
2026-01-23 17:11:22 -0800

    force raid to be in hard mode
    Signed-off-by: AS_Allen_Shih <65409535+ASAllenShih@users.noreply.github.com>

--- src/main/java/tw/asts/mc/asts/event/Event.java
+++ src/main/java/tw/asts/mc/asts/event/Event.java
@@ -1,15 +1,21 @@
 package tw.asts.mc.asts.event;
 
+import net.minecraft.world.entity.raid.Raid;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockDropItemEvent;
 import org.bukkit.event.entity.EntityTargetEvent;
+import org.bukkit.event.raid.RaidTriggerEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import tw.asts.mc.asts.util.Log;
 import tw.asts.mc.asts.util.UserConfig;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 final public class Event implements Listener {
     private final MobAttack mobAttack;
@@ -31,5 +37,18 @@ final public class Event implements Listener {
     @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
     public void onBlockDropItem(BlockDropItemEvent event) {
         autoInvPick.onBlockDropItem(event);
+    }
+    @EventHandler
+    public void onRaidTrigger(RaidTriggerEvent event) {
+        try {
+            org.bukkit.Raid bukkitRaid = event.getRaid();
+            Method getHandle = bukkitRaid.getClass().getMethod("getHandle");
+            Object nmsRaid = getHandle.invoke(bukkitRaid);
+
+            Field numGroupsField = nmsRaid.getClass().getDeclaredField("numGroups");
+            numGroupsField.setAccessible(true);
+            numGroupsField.setInt(nmsRaid, 7);
+        }catch (Exception e){
+        }
     }
 }
